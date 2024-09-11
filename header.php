ed8b6ce666c68f14341f9434241f0b3208a7648e
reasonsx <kamkre01@easv365.dk>
2024-09-11 15:11:23 +0200

    Update header.php

--- header.php
+++ header.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
-
-<>
+<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Life Below Water</title>
+    <title><?php wp_title(); ?></title>
     <?php wp_head(); ?>
 </head>
 
