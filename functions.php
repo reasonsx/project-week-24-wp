f0019da8524457786f198519ab82dc46c6e98c7b
reasonsx <kamkre01@easv365.dk>
2024-09-11 13:25:25 +0200

    ok

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 function my_custom_theme_scripts()
 {
-    wp_enqueue_style('main-style', get_template_directory_uri() . '/css/style.css');
+    wp_enqueue_style('main-style', get_template_directory_uri() . 'style.css');
 }
 
 add_action('wp_enqueue_scripts', 'my_custom_theme_scripts');
